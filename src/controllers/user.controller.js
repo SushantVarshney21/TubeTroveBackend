f90f0ffcc635971f41df247486afed26646b6814
SushantVarshney21 <sushantvars@gmail.com>
2024-01-11 21:21:34 -1100

    solve some bugs in index.js , userController and AsyncHandler

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,11 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 
-const registerUser = asyncHandler(async (req,res) => {
-    res.status(200).json({
-        message:"ok"
+const registerUser = asyncHandler( async (req, res) => {
+    res.status(500).json({
+        message: "Mast chal raha hu bhai"
     })
-})
+} )
+
 
 export {registerUser}
